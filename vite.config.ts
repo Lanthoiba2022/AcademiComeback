ac05d85331189625fa76890a91f5daf67c584453
Lanthoiba22 <khumanthemlanthoiba@gmail.com>
2025-06-22 10:45:49 +0530

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,18 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  build: {
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,   // Removes console.log, console.debug, etc.
+        drop_debugger: true   // Removes debugger statements
+      }
+    }
+  }
 });
